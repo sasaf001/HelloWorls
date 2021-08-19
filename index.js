2cda29cf6ee3fed6e155dc097dd48a23f4b74a9c
sasaf001 <85259559+sasaf001@users.noreply.github.com>
2021-08-19 16:18:20 +0200

    Continue with API. 2 API's added to JS

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     init()
 
     function init(){
@@ -30,25 +30,78 @@ $(document).ready(function(){
         $('#data').empty()
     }
 
-    document.addEventListener('DOMContentLoaded',()=>{
-
-        const selectDrop= document.getElementById('countries');
-        
-        fetch('https://restcountries.eu/rest/v2/all').then(res=>{
-            return res.json();
-        }).then(data =>{
-            let output="";
-            data.forEach(country => {
-               output+= `<option value=${country.name}>${country.name}</option>`;
-            })
-            selectDrop.innerHTML=output;
-            
-            })
-        }).catch(err =>{
-            console.log(err);
-        })
-
     
+
+    const url = 'https://api.covid19api.com/summary';
+    fetch(url)
+    .then((response)=>{
+        response.json()
+        .then((result)=>{console.log('result', result)
+        })
+    })
+    .catch(function(error){
+        console.log(error)
+       
+    })
+    const checkIpWrapper = document.getElementsByClassName('ip')[0];
+*/
+document.addEventListener('DOMContentLoaded',()=>{
+
+    const selectDrop= document.getElementById('countries');
+    
+    fetch('https://restcountries.eu/rest/v2/all').then(res=>{
+        return res.json();
+    }).then(data =>{
+        let output="";
+        data.forEach(country => {
+           output+= `<option value=${country.name}>${country.name}</option>`;
+        })
+        selectDrop.innerHTML=output;
+        
+        }).catch(err =>{
+        console.log(err);
+    })
+    })
+
+const checkIpBtn = document.getElementById('suchCountry'); 
+    checkIpBtn.addEventListener('click' , function () {
+        const thisElement = this;
+        thisElement.setAttribute("disabled" , "disabled");
+ 
+        const url = new XMLHttpRequest();
+        url.open("GET","https://api.covid19api.com/summary");
+        url.responseType = "json"
+        url.onreadystatechange = function(e) {
+      
+            if (this.status === 200 && this.readyState == this.DONE){
+      
+                //thisElement.remove();
+                //const table = document.querySelector(".ip table");
+                table.classList.toggle('ds-none');
+      
+                const data = this.response;
+                const countryData = [data.Countries.name, data.Global.TotalConfirmed , data.Global.TotalRecovered , data.TotalDeaths ]
+      
+                const tdTableElement = document.querySelectorAll('.ip table td');
+      
+                let counter = 0;
+                for(const element of tdTableElement){
+                    element.innerHTML = countryData[counter];
+                    counter++;
+                }
+      
+            }
+                  
+            else if(this.status < 200 && this.readyState == this.DONE){
+                window.alert("erroe");
+                console.error("ERR");
+            }
+        }
+      
+        url.send();
+      
+    });
+  
 
 const tarikh = new Date();
 document.getElementById("datum").innerHTML = tarikh.toDateString();
