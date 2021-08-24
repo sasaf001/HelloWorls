a24a2955025f57dfe70625a71d3d20d79183cadb
sasaf001 <85259559+sasaf001@users.noreply.github.com>
2021-08-24 11:59:48 +0200

    Countries are corrected

--- index.js
+++ index.js
@@ -1,108 +1,71 @@
-/*$(document).ready(function(){
-    init()
+document.addEventListener('DOMContentLoaded', () => {
+    const selectDrop = document.getElementById('countries');
+    fetch('https://api.covid19api.com/summary')
+        .then(response => {
+            return response.json();
 
-    function init(){
-    var url ="https://api.covid19api.com/summary"
+        }).then(data => {
+            console.log(data);
+            let output = "";
+            data.Countries.forEach(country => {
+                output += `<option value=${country.Country}>${country.Country}</option>`;
 
-    
-    var data=''
-    
-    $.get(url,function(data){
-        console.log(data.Global)
+            })
+            selectDrop.innerHTML = output;
 
-        data=`
-        
-        <td></td>
-        <td>${data.Global.TotalConfirmed}</td>
-        <td>${data.Global.Totalrecovered}</td>
-        <td>${data.Global.TotalDeaths}</td>
-        
 
-        `
-        $("#data").html(data)
-    })}
+        }).catch(err => {
+            console.log(err)
+        })
+
 })
-    function refreshData(){
-        cleardata()
-        init()
-    }
-    function clearData(){
-        $('#data').empty()
-    }
+document.getElementById("suchCountry").addEventListener("click", myfunction);
 
-    
+function myfunction() {
+    document.getElementById("land").innerHTML = getSelection(output);
+}
 
-    const url = 'https://api.covid19api.com/summary';
-    fetch(url)
-    .then((response)=>{
-        response.json()
-        .then((result)=>{console.log('result', result)
-        })
-    })
-    .catch(function(error){
-        console.log(error)
-       
-    })
-    const checkIpWrapper = document.getElementsByClassName('ip')[0];
-*/
-document.addEventListener('DOMContentLoaded',()=>{
 
-    const selectDrop= document.getElementById('countries');
-    
-    fetch('https://restcountries.eu/rest/v2/all').then(res=>{
-        return res.json();
-    }).then(data =>{
-        let output="";
-        data.forEach(country => {
-           output+= `<option value=${country.name}>${country.name}</option>`;
-        })
-        selectDrop.innerHTML=output;
-        
-        }).catch(err =>{
-        console.log(err);
-    })
-    })
 
-const checkIpBtn = document.getElementById('suchCountry'); 
-    checkIpBtn.addEventListener('click' , function () {
-        const thisElement = this;
-        thisElement.setAttribute("disabled" , "disabled");
- 
-        const url = new XMLHttpRequest();
-        url.open("GET","https://api.covid19api.com/summary");
-        url.responseType = "json"
-        url.onreadystatechange = function(e) {
-      
-            if (this.status === 200 && this.readyState == this.DONE){
-      
-                //thisElement.remove();
-                //const table = document.querySelector(".ip table");
-                table.classList.toggle('ds-none');
-      
-                const data = this.response;
-                const countryData = [data.Countries.name, data.Global.TotalConfirmed , data.Global.TotalRecovered , data.TotalDeaths ]
-      
-                const tdTableElement = document.querySelectorAll('.ip table td');
-      
-                let counter = 0;
-                for(const element of tdTableElement){
-                    element.innerHTML = countryData[counter];
-                    counter++;
-                }
-      
+
+
+/*const checkIpBtn = document.getElementById('suchCountry');
+checkIpBtn.addEventListener('click', function() {
+    const thisElement = this;
+    //thisElement.setAttribute("disabled", "disabled");
+
+    const url = new XMLHttpRequest();
+    url.open("GET", "https://api.covid19api.com/summary");
+    url.responseType = "json"
+    url.onreadystatechange = function(e) {
+
+        if (this.status === 200 && this.readyState == this.DONE) {
+
+            //thisElement.remove();
+            const table = document.querySelector(".ip table");
+            table.classList.toggle('ds-none');
+
+            const data = this.response;
+            console.log(data)
+            const countryData = [data.Countries[1].country, data.Global.TotalConfirmed, data.Global.TotalRecovered, data.TotalDeaths]
+
+            const tdTableElement = document.querySelectorAll('.ip table td');
+
+            let counter = 0;
+            for (const element of tdTableElement) {
+                element.innerHTML = countryData[counter];
+                counter++;
             }
-                  
-            else if(this.status < 200 && this.readyState == this.DONE){
-                window.alert("erroe");
-                console.error("ERR");
-            }
+
+        } else if (this.status < 200 && this.readyState == this.DONE) {
+            window.alert("erroe");
+            console.error("ERR");
         }
-      
-        url.send();
-      
-    });
-  
 
+
+    }
+    url.send();
+
+});*/
 const tarikh = new Date();
 document.getElementById("datum").innerHTML = tarikh.toDateString();
-
