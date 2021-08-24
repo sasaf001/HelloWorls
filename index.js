943607f679e7d525b83b75279b05fb891acb9134
sasaf001 <85259559+sasaf001@users.noreply.github.com>
2021-08-24 16:56:11 +0200

    start with Rapid API

--- index.js
+++ index.js
@@ -1,71 +1,47 @@
-document.addEventListener('DOMContentLoaded', () => {
+/*document.addEventListener('DOMContentLoaded', () => {
     const selectDrop = document.getElementById('countries');
     fetch('https://api.covid19api.com/summary')
         .then(response => {
             return response.json();
-
         }).then(data => {
             console.log(data);
             let output = "";
             data.Countries.forEach(country => {
                 output += `<option value=${country.Country}>${country.Country}</option>`;
-
             })
             selectDrop.innerHTML = output;
-
-
         }).catch(err => {
             console.log(err)
         })
 
 })
-document.getElementById("suchCountry").addEventListener("click", myfunction);
+var selectland = document.getElementById('countries');
+var submitbtn = document.getElementById('suchCountry');
 
-function myfunction() {
-    document.getElementById("land").innerHTML = getSelection(output);
+
+submitbtn.onclick = function() {
+    document.getElementById("land").innerHTML = selectland.value;
+    document.getElementById("leute").innerHTML = "1";
+    document.getElementById("heil").innerHTML = "2";
+    document.getElementById("tod").innerHTML = "3";
 }
 
 
-
-
-
-/*const checkIpBtn = document.getElementById('suchCountry');
-checkIpBtn.addEventListener('click', function() {
-    const thisElement = this;
-    //thisElement.setAttribute("disabled", "disabled");
-
-    const url = new XMLHttpRequest();
-    url.open("GET", "https://api.covid19api.com/summary");
-    url.responseType = "json"
-    url.onreadystatechange = function(e) {
-
-        if (this.status === 200 && this.readyState == this.DONE) {
-
-            //thisElement.remove();
-            const table = document.querySelector(".ip table");
-            table.classList.toggle('ds-none');
-
-            const data = this.response;
-            console.log(data)
-            const countryData = [data.Countries[1].country, data.Global.TotalConfirmed, data.Global.TotalRecovered, data.TotalDeaths]
-
-            const tdTableElement = document.querySelectorAll('.ip table td');
-
-            let counter = 0;
-            for (const element of tdTableElement) {
-                element.innerHTML = countryData[counter];
-                counter++;
-            }
-
-        } else if (this.status < 200 && this.readyState == this.DONE) {
-            window.alert("erroe");
-            console.error("ERR");
-        }
-
-
-    }
-    url.send();
-
-});*/
 const tarikh = new Date();
-document.getElementById("datum").innerHTML = tarikh.toDateString();
+document.getElementById("datum").innerHTML = tarikh.toDateString();*/
+fetch("https://covid-193.p.rapidapi.com/countries", {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "covid-193.p.rapidapi.com",
+            "x-rapidapi-key": "fc2160d35amsh279ce92aef9439fp1d6d32jsned4ac5acd398"
+        }
+    })
+    .then(response => {
+        console.log(response);
+    })
+    .then({
+
+    })
+    .catch(err => {
+        console.error(err);
+    });
