dc6bee2eeb94bf8499286d42c2a3c38f6eeb9ab7
sasaf001 <85259559+sasaf001@users.noreply.github.com>
2021-08-26 15:30:31 +0200

    new API is added

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 /*document.addEventListener('DOMContentLoaded', () => {
     const selectDrop = document.getElementById('countries');
+    
     fetch('https://api.covid19api.com/summary')
         .then(response => {
             return response.json();
         }).then(data => {
             console.log(data);
             let output = "";
+            
             data.Countries.forEach(country => {
                 output += `<option value=${country.Country}>${country.Country}</option>`;
+                
             })
-            selectDrop.innerHTML = output;
-        }).catch(err => {
-            console.log(err)
         })
-
+    selectDrop.innerHTML = output;
 })
+
+.catch(err => {
+    console.log(err)
+})
+
+
 var selectland = document.getElementById('countries');
 var submitbtn = document.getElementById('suchCountry');
 
@@ -25,23 +31,75 @@ submitbtn.onclick = function() {
     document.getElementById("heil").innerHTML = "2";
     document.getElementById("tod").innerHTML = "3";
 }
+let n = data.Countries.TotalConfirmed;
+console.log(n);*/
+
+
+var myHeaders = new Headers();
+myHeaders.append("x-rapidapi-host", "covid-19-coronavirus-statistics.p.rapidapi.com");
+myHeaders.append("x-rapidapi-key", "fc2160d35amsh279ce92aef9439fp1d6d32jsned4ac5acd398");
+
+var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+};
+document.addEventListener('DOMContentLoaded', () => {
+    const selectDrop = document.getElementById('countries');
+    fetch("https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?=", requestOptions)
+
+    .then(response => {
+            return response.json();
+
+        })
+        .then(data => {
+            console.log(data);
+            let output = "";
+            data.covid19Stats.forEach(country => {
+                output += `<option value=${country.country}>${country.country}</option>`;
+            })
+        })
+    selectDrop.innerHTML = output;
+})
+
+.catch(error => console.log('error', error));
+
+
+
+
+
+
+
+var xValues = ["TotalConfirmed", "Totalrecovered", "TotalDeaths"];
+var yValues = [55, 49, 44];
+var barColors = [
+    "#2b5797",
+    "#00aba9",
+    "#b91d47"
+];
+//update az tarighe data table
+const chart = new Chart("myChart", {
+
+    type: "pie",
+    data: {
+        labels: xValues,
+        datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: "Statistics of Corona on Pie Chart"
+
+        }
+    }
+});
+
+
+
 
 
 const tarikh = new Date();
-document.getElementById("datum").innerHTML = tarikh.toDateString();*/
-fetch("https://covid-193.p.rapidapi.com/countries", {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "covid-193.p.rapidapi.com",
-            "x-rapidapi-key": "fc2160d35amsh279ce92aef9439fp1d6d32jsned4ac5acd398"
-        }
-    })
-    .then(response => {
-        console.log(response);
-    })
-    .then({
-
-    })
-    .catch(err => {
-        console.error(err);
-    });
+document.getElementById("datum").innerHTML = tarikh.toDateString();
